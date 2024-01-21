d627e22fb88c7cf18cc3a2798fcdf39538f64595
iMartzen <git@martzen.nl>
2024-01-21 12:21:12 +0100

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,154 @@
+{
+  "1": {
+    "group": "Trust",
+    "questions": "Team members admit their mistakes."
+  },
+  "2": {
+    "group": "Conflict",
+    "questions": "Team members are passionate and unguarded in their discussion of issues."
+  },
+  "3": {
+    "group": "Results",
+    "questions": "Team members are quick to point out the contributions and achievements of others."
+  },
+  "4": {
+    "group": "Conflict",
+    "questions": "Team meetings are interesting and compelling (not boring)."
+  },
+  "5": {
+    "group": "Conflict",
+    "questions": "During team meetings, the most important—and difficult—issues are discussed."
+  },
+  "6": {
+    "group": "Trust",
+    "questions": "Team members acknowledge their weaknesses to one another."
+  },
+  "7": {
+    "group": "Conflict",
+    "questions": "Team members voice their opinions even at the risk of causing disagreement."
+  },
+  "8": {
+    "group": "Accountability",
+    "questions": "Team members point out one another’s unproductive behaviors."
+  },
+  "9": {
+    "group": "Results",
+    "questions": "The team has a reputation for high performance."
+  },
+  "10": {
+    "group": "Trust",
+    "questions": "Team members ask for help without hesitation."
+  },
+  "11": {
+    "group": "Commitment",
+    "questions": "Team members leave meetings confident that everyone is committed to the decisions that were agreed upon."
+  },
+  "12": {
+    "group": "Conflict",
+    "questions": "During discussions, team members challenge one another about how they arrived at their conclusions and opinions."
+  },
+  "13": {
+    "group": "Trust",
+    "questions": "Team members ask one another for input regarding their areas of responsibility."
+  },
+  "14": {
+    "group": "Results",
+    "questions": "When the team fails to achieve collective goals, each member takes personal responsibility to improve the team’s performance."
+  },
+  "15": {
+    "group": "Results",
+    "questions": "Team members willingly make sacrifices in their areas for the good of the team."
+  },
+  "16": {
+    "group": "Accountability",
+    "questions": "Team members are quick to confront peers about problems in their respective areas of responsibility."
+  },
+  "17": {
+    "group": "Trust",
+    "questions": "Team members acknowledge and tap into one another’s skills and expertise."
+  },
+  "18": {
+    "group": "Conflict",
+    "questions": "Team members solicit one another’s opinions during meetings."
+  },
+  "19": {
+    "group": "Commitment",
+    "questions": "Team members end discussions with clear and specific resolutions and calls to action."
+  },
+  "20": {
+    "group": "Accountability",
+    "questions": "Team members question one another about their current approaches and methods."
+  },
+  "21": {
+    "group": "Accountability",
+    "questions": "The team ensures that poor performers feel pressure and the expectation to improve."
+  },
+  "22": {
+    "group": "Trust",
+    "questions": "Team members willingly apologize to one another."
+  },
+  "23": {
+    "group": "Conflict",
+    "questions": "Team members communicate unpopular opinions to the group."
+  },
+  "24": {
+    "group": "Commitment",
+    "questions": "The team is clear about its direction and priorities."
+  },
+  "25": {
+    "group": "Results",
+    "questions": "Team members are slow to seek credit for their own contributions."
+  },
+  "26": {
+    "group": "Accountability",
+    "questions": "All members of the team are held to the same high standards."
+  },
+  "27": {
+    "group": "Conflict",
+    "questions": "When conflict occurs, the team confronts and deals with the issue before moving to another subject."
+  },
+  "28": {
+    "group": "Commitment",
+    "questions": "The team is aligned around common objectives."
+  },
+  "29": {
+    "group": "Results",
+    "questions": "The team consistently achieves its objectives."
+  },
+  "30": {
+    "group": "Commitment",
+    "questions": "The team is decisive, even when perfect information is not available."
+  },
+  "31": {
+    "group": "Results",
+    "questions": "Team members value collective success more than individual achievement."
+  },
+  "32": {
+    "group": "Trust",
+    "questions": "Team members are unguarded and genuine with one another."
+  },
+  "33": {
+    "group": "Trust",
+    "questions": "Team members can comfortably discuss their personal lives with one another."
+  },
+  "34": {
+    "group": "Commitment",
+    "questions": "The team sticks to decisions."
+  },
+  "35": {
+    "group": "Accountability",
+    "questions": "Team members consistently follow through on promises and commitments."
+  },
+  "36": {
+    "group": "Accountability",
+    "questions": "Team members offer unprovoked, constructive feedback to one another."
+  },
+  "37": {
+    "group": "Results",
+    "questions": "Team members place little importance on titles and status. (A high score on this statement indicates that titles and status are NOT important to team members.)"
+  },
+  "38": {
+    "group": "Commitment",
+    "questions": "Team members support group decisions even if they initially disagreed."
+  }
+}
