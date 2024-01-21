cae3c9e6d8d5e45e68306382106ccb8cb7d8a267
iMartzen <git@martzen.nl>
2024-01-21 12:22:09 +0100

    Create test-questions.js

---
+++ test/test-questions.js
@@ -0,0 +1,22 @@
+{
+  "1": {
+    "group": "Trust",
+    "questions": "Team members admit their mistakes."
+  },
+  "2": {
+    "group": "Trust",
+    "questions": "Team members are passionate and unguarded in their discussion of issues."
+  },
+  "3": {
+    "group": "Trust",
+    "questions": "Team members are quick to point out the contributions and achievements of others."
+  },
+  "4": {
+    "group": "Results",
+    "questions": "Team meetings are interesting and compelling (not boring)."
+  },
+  "5": {
+    "group": "Accountability",
+    "questions": "During team meetings, the most important—and difficult—issues are discussed."
+  }
+}
